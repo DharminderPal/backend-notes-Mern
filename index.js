7fae7612d53a2e39bb8d439a5d4fe513279ca85a
Dharminder Pal (CBT) <cbt.sd4@deepakfasteners.in>
2025-11-29 10:24:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// import express from 'express'
+
+// const app = express();
+// //this is called 
+// app.get('/',()=>{
+//     console.log()
+// })
+
+// // deployed on a port 
+// // PORT---->>network ports --> interfaces thrugh  which our applications acces the network 
+
+// app.get('/',(req,res)=>{
+//     console.log("root called ")
+//     res.send("welcome to the root router "
+
+//     )
+// })
+
+// app.listen(
+//     3000,()=>{
+//         console.log("the server has been live ")
+//     }
+// )
+
+//event driven -->> some event will trigger than a funciton will  run 
+// req-> funciton------ res 
+// document.addEventListener("click")
+
+// espress provide us with a very clean way og handling what we recieve as a request anf what we caan send back as a response 
+// req, res => request and response objects that are propvided by express
+
+//https://lcoalhost:3000/
+
+
+
+import express from "express"
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
+
+
+app.get("/request",(req,res)=>{
+  const requestdeatails ={
+    
+  }
+})
+
+
+
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+              
+
+
---
+++ into.js
@@ -0,0 +1,68 @@
+/*
+mern stack 
+
+https protocol --- >> internet protocol used for communication in the appliaction layer of networking  
+
+Http Methods--->> request -> purpose in terms of data -->get ,post ,put ,patch ,delete
+
+restful api -->> Reprasetational satete transfer -> this is an api development architecture thah follows some priciples that help 
+us to rcreate a scalable flxible and efficient web server for communitcation between  cleint and data 
+
+architecture pointers
+1. unifirms interface----> well identification of resorces  -> /api /asignup /
+2. staetlessness ->>>> we dnot store any data on the server.
+3. cacheability -->> the server should be able to respone for better performance ... and reduce latency ..
+4. layered system -->>   we implemnt feautres in layers .
+
+
+http status codes  
+they tell about the nature  of the response that we have got 
+
+200 -->ok
+400 ---> client errors 
+500 server errors 
+
+react -> framwork that is used for ceating dynamic uis and front ends.
+node.js --> javascript runtime that allows us to run javascript on the server side , enabling 
+non blocking i/o operations for building scalable network applications. 
+
+
+
+*/
+
+
+//npm - node package manager   this is hlelp an instaling manging and shipping our sever side applicaiton
+
+
+
+// express.js --->> is a fast umopiniated, minilist web framework for node.js that hleps in building 
+// apis for server side dvelopmemtn which inclides rotingh midleware hanldig and http ee/
+// processing 
+
+// serverr is event drive ---->>> whenver we will get a request inly then a fnciton will br invoked 
+// routes  handlers --->> 
+
+// step to step express.js 
+// pacjkage-loack -->> this stores the stable version sof the package that we are using inside our applications 
+// node_modules ->> files of the packages are stored 
+// cacaheablility the server should 
+
+
+
+
+// 
+
+
+// 1. npm install 
+// how  to set  
+
+//request  server --->>>
+
+// routes handlers
+
+
+
+
+
+
+
